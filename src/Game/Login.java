b1672de55c6844604149a188081e48e59a38f2d3
Ali Al Dobyan <11098813@studentmail.ul.ie>
2018-11-08 17:58:11 +0000

    Login java file for testing.

---
+++ src/Game/Login.java
@@ -0,0 +1,22 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Game;
+import ttt.james.server.TTTWebService;
+import ttt.james.server.TTTWebService_Service;
+/**
+ *
+ * @author alialdobyan
+ */
+public class Login {
+    
+    public static void main(String[]args){
+        TTTWebService_Service service = new TTTWebService_Service();
+         TTTWebService proxy = service.getTTTWebServicePort();
+         
+        
+    }
+    
+}
